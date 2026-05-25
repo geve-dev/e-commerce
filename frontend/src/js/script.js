f2ffb72d4721983c1ed693de2315a2226a5d3c90
GABRIEL VINICIUS DE CAMPOS DA SILVA <gabriel_silva175@estudante.sesisenai.org.br>
2026-05-25 17:11:59 -0300

    login sendo implementando

--- frontend/src/js/script.js
+++ frontend/src/js/script.js
@@ -1,3 +1,17 @@
+const btnAbrir = document.getElementById('btnAbrir')
+const btnCancel = document.getElementById('btnCancel');
+const popup     = document.getElementById('popupOverlay')
+
+btnCancel.addEventListener('click', () => {
+  popup.classList.remove('active');
+})
+
+document.querySelector('header').addEventListener('click', (event) => {
+    if (event.target.id === 'btnAbrir') {
+        popup.classList.add('active');
+    }
+});
+
 getProducts()
 
 async function getProducts() {
@@ -12,6 +26,7 @@ async function getProducts() {
 }
 
 async function renderProducts(dados) {
+  console.log(dados);
   const produtos = document.querySelector('.produtos');
   let html = '';
 
@@ -23,19 +38,55 @@ async function renderProducts(dados) {
     const image      = dados[i].image;
     
     html += `
-    <div classe="produtos-header">>
-          <img src="${image}" alt="">
+    <div class="produto">
+      <div class="produto-header">>
+            <img src="${image}" alt="">
       </div>
-      
-      <div classe="produtos-main">>
-          <p>${name}</p>
-          <span>${price}</span>
-      </div>
-
-      <button type="button">+</button>
+        
+        <div class="produto-main">>
+            <p>${name}</p>
+            <span>${price}</span>
+        </div>
+  
+        <button onClick="addToItems(${id_product})">+</button>
     </div>
     `;
   }
 
   produtos.innerHTML = html;
+}
+
+async function addToItems(id_product) {
+  const res = await fetch(`http://localhost:3003/item`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ id_product, quantity: 1 }),
+    
+  })
+
+  const data = await res.json();
+  alert(data.message);
+}
+
+async function login() {
+  const form = document.getElementById('fl');
+  const dados = new FormData(form);
+  const valores = Object.fromEntries(dados.entries());
+  console.log(valores)
+
+  
+  const res = await fetch(`http://localhost:3003/auth/login`, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(valores)
+  })
+  
+  const data = await res.json();
+    
+  if (data.token) {
+    localStorage.setItem('token', data.token);
+    alert('Logado com sucesso!');
+  }
 }
